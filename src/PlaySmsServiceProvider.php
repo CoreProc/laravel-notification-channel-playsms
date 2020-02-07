5f195228ea77c9fecf12a317a48c12a0be9d92d0
Chris Bautista <chrisbjr@gmail.com>
2020-02-07 10:47:18 +0800

    Remove publishing of config file

--- src/PlaySmsServiceProvider.php
+++ src/PlaySmsServiceProvider.php
@@ -7,16 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class PlaySmsServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        $this->publishes([
-            __DIR__.'/config/playsms.php' => config_path('playsms.php'),
-        ]);
-    }
-
-    /**
      * Register the application services.
      */
     public function register()
